bf6312f065130df1338abdf253aac89bcd1dd738
sumj25 <sumanj252001@gmail.com>
2023-05-04 14:44:31 +0530

    add install reffer

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import { getInstallReferrer } from '@use-expo/application';
 
 export default function App() {
+  const [referrer, setReferrer] = useState('');
+
+  useEffect(() => {
+    async function getReferrer() {
+      try {
+        const referrer = await getInstallReferrer();
+        setReferrer(referrer);
+        console.log('Install Referrer:', referrer);
+      } catch (e) {
+        console.log('Error retrieving install referrer:', e);
+      }
+    }
+
+    getReferrer();
+  }, []);
+ 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Install Referrer: {referrer}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
